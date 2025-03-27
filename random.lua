f184cf0b38ed6bf3ff29eaf73b4be1dc2bf1c59f
Michael Savin <jtprogru@gmail.com>
2025-03-27 10:37:52 +0300

    fix: Change formatting for lua code
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- random.lua
+++ random.lua
@@ -1,6 +1,7 @@
 math.randomseed(os.time())
+
 request = function()
-    local random_value = math.random(1, 1000)  -- Random number between 1 and 1000
-    local path = "http://localhost:8080/cubic-root" .. "?d=" .. random_value
-    return wrk.format(nil, path)
+  local random_value = math.random(1000000, 1000000000) -- Random number between 1kk and 1kkk
+  local path = "http://localhost:8080/cubic-root" .. "?d=" .. random_value
+  return wrk.format(nil, path)
 end
